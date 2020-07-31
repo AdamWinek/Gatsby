aace674e15285c7df2a7ad5bf764213877432dc3
reonrash <45687245+reonrash@users.noreply.github.com>
2020-07-31 09:45:33 -0400

    Styling and scss structure correction

--- gatsby-site/src/components/LandingPage.js
+++ gatsby-site/src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import '../styles/index.scss'
 
 import style from "../styles/LandingPage.module.scss"
 function LandingPage() {
@@ -9,9 +10,8 @@ function LandingPage() {
 
             </div>
             <div className={style.missionDiv}>
-                <h1 className={style.missionH1}> Mission </h1>
+                <h1 className={style.missionH1}> Mission Statement</h1>
                 <div className={style.missionBox}>
-                    <div className={style.lineVector}></div>
                     <p className={style.missionP}> My mission is to provide quality therapeutic services to families, couples and individuals. I assist my clients in improving their relationships and creating positive changes in their lives. Working from a holistic and developmental perspective, I assist clients to develop their full potential in order to thrive.
                     </p>
                 </div>
