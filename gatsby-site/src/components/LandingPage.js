00e03050fd57db7036773dd5e3a30567614496f4
reonrash <45687245+reonrash@users.noreply.github.com>
2020-08-01 22:10:48 -0400

    landing page image

--- gatsby-site/src/components/LandingPage.js
+++ gatsby-site/src/components/LandingPage.js
@@ -36,25 +36,25 @@ function LandingPage() {
         ({ item, key, props }) =>
           item && (
             <animated.div key={key} style={props} className={style.animated}>
-            <div className={style.center}>
-              <Topnav setIsNav={() => setIsNav(true)} isNav={isNav} />
+              <div className={style.center}>
+                <Topnav setIsNav={() => setIsNav(true)} isNav={isNav} />
 
-              <div className={style.missionDiv}>
-                <h1 className={style.missionH1}> Mission Statement</h1>
-                <div className={style.missionBox}>
-                  <p className={style.missionP}>
-                    {" "}
-                    My mission is to provide quality therapeutic services to
-                    families, couples and individuals. I assist my clients in
-                    improving their relationships and creating positive changes
-                    in their lives. Working from a holistic and developmental
-                    perspective, I assist clients to develop their full
-                    potential in order to thrive.
-                  </p>
+                <div className={style.missionDiv}>
+                  <h1 className={style.missionH1}> Mission Statement</h1>
+                  <div className={style.missionBox}>
+                    <p className={style.missionP}>
+                      {" "}
+                      My mission is to provide quality therapeutic services to
+                      families, couples and individuals. I assist my clients in
+                      improving their relationships and creating positive
+                      changes in their lives. Working from a holistic and
+                      developmental perspective, I assist clients to develop
+                      their full potential in order to thrive.
+                    </p>
+                  </div>
+                  <button className={style.button}>Get in Contact</button>
                 </div>
-                <button className={style.button}>Get in Contact</button>
-              </div>
-              <About />
+                <About />
               </div>
             </animated.div>
           )
